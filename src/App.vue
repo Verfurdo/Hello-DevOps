a9c0bb49096893e5e37126a51587c2182abb532c
Verfurdo <amolnart@gmail.com>
2026-05-26 23:43:44 +0200

    Add version

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <h1>Hello DevOps beadandó feladat</h1>
+    <h1>Hello DevOps beadandó feladat - Version 1.0</h1>
     <p>Vue + Docker + CI + Deploy</p>
   </div>
 </template>
