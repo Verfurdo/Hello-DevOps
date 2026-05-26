5a8a8f02931f39fa44649fe4b8a25dc192fd808d
Verfurdo <amolnart@gmail.com>
2026-05-26 23:27:25 +0200

    Initial Vue app

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Hello DevOps beadandó feladat</h1>
+    <p>Vue + Docker + CI + Deploy</p>
+  </div>
+</template>
+
+
+<style scoped></style>
+
+
