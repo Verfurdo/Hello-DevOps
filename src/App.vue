99f5bbadf34fb4ad6a67629f438bcfe40b229759
Verfurdo <amolnart@gmail.com>
2026-05-27 00:07:53 +0200

    Added Vue homepage layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
-<script setup></script>
-
 <template>
-  <div>
+  <div class="container">
     <h1>Hello DevOps beadandó feladat - Version 1.0</h1>
     <p>Vue + Docker + CI + Deploy</p>
+
+    <img src="https://vuejs.org/images/logo.png" alt="Vue Logo" />
+
+    <h2>Vue</h2>
+    <p>Version 1.0</p>
   </div>
 </template>
 
+<style>
+body {
+  margin: 0;
+  background: black;
+  color: white;
+  font-family: Arial, sans-serif;
+}
 
-<style scoped></style>
+.container {
+  height: 100vh;
 
+  display: flex;
+  flex-direction: column;
 
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+}
+
+img {
+  width: 180px;
+}
+</style>
